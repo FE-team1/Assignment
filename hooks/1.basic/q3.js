8805f98adabc600812fd6d0b9a031e293caa58f1
kiminn <808010x@gmail.com>
2023-08-31 20:45:51 +0900

    q1~q3 문제풀이

--- hooks/1.basic/q3.js
+++ hooks/1.basic/q3.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import Q3components from "../../components/1.basic/q3components";
 import { useEffect } from "react";
 // https://velog.io/@dianestar/JavaScript-React-React%EC%97%90%EC%84%9C-setInterval%EC%9D%98-%ED%99%9C%EC%9A%A9
@@ -19,21 +20,22 @@ const [count, setCount] = useState(0);
 const [btn, setBtn] = useState(false);
 const [pause, setPause] = useState(false);
 
+const handleResume = () => {
+  setBtn(!btn)
+  setCount(0)
+};
 
-
-// useEffect(() => {
-//   const timer = setInterval(() => {
-//     console.log(count)
-//     // setCount(count + 1);
-//     setCount(count => count + 1);
-//   }, 2000);
-// }, [count]);
-
-// const stop = () => {
-//   if (pause) {
-//     setPause(true);
-//   }
-// }
+let timer;
+useEffect(() => {
+  if(btn){
+    console.log('타이머 진행중')
+    timer = setInterval(() => {
+    setCount((count)=> count + 1);
+    },2000);
+  }
+  return() => clearInterval(timer);
+},[btn])
+// 의존성 배열 : btn이 변경될 때만 호출된다
 
 
 
@@ -44,15 +46,17 @@ const [pause, setPause] = useState(false);
         <p> 줄넘기 횟수 : {count} </p>
         {btn ? '' : <Q3components/>}
         <p>
-          <button onClick={() => {
-            setBtn(!btn);
-          }}>줄넘기 시작</button>
+          {!btn && <Q3components/>}
+          {/* button의 onClick에 같은 이벤트 함수 놓음으로써 해결! */}
+          <button onClick={handleResume}>줄넘기 시작</button>
         </p>
         <p>
-          {/* <button onClick={stop}>줄넘기 중지</button> */}
+          <button onClick={handleResume}>줄넘기 중지</button>
         </p>
       </div>
     </>
   );
-}
+
+};
+
 export default Q3;
